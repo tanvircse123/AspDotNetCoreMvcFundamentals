d5ef1e8c75c1c4c1b03cfc3b42c1f94a645721d7
tanvircse123 <tanvir.cse.cuet@gmail.com>
2023-04-09 06:07:51 +0600

    added

---
+++ API_DEEP_DIVE/CourseLibrary.API/Helpers/DateTimeOffsetExtensions.cs
@@ -0,0 +1,28 @@
+﻿
+namespace CourseLibrary.API.Helpers;
+public static class DateTimeOffsetExtensions
+{
+    public static int GetCurrentAge(this DateTimeOffset dateTimeOffset)
+    {
+        /*
+        This "if" statement checks if the current date is earlier than 
+        the date equivalent to the input date with the age in years added.
+        If the current date is earlier, it means the person has not yet 
+        celebrated their birthday this year, so we need to subtract 1 from
+        the age to get the correct age. Therefore, the "age" variable is
+        decremented by 1.
+        
+        
+        */
+        var currentDate = DateTime.UtcNow;
+        int age = currentDate.Year - dateTimeOffset.Year;
+
+        if (currentDate < dateTimeOffset.AddYears(age))
+        {
+            age--;
+        }
+
+        return age;
+    }
+}
+
